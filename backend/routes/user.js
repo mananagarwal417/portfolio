5885abf4fe41b495fe7597ccb90811c5878396db
mananagarwal417 <agarwalmanan73@gmail.com>
2026-05-01 21:07:28 +0530

    added gitignore and cleaned repo

--- backend/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const User = require("../models/user");
-const { handlePostRequest } = require("../controllers/userRoute");
-
-router.route("/contact").post(handlePostRequest);
-
-module.exports = router;
